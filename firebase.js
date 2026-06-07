2bd8ec82d8236933aa8847866760ce99d2f6295c
GODSGIFT IYATSE TAMARADENYEFA <godsgiftiyatse@gmail.com>
2026-06-07 11:05:12 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,21 +1,17 @@
-import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
-import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBoQlykPV5JYQPLj96D2yLpFJ0pWdtSLiU",
-  authDomain: "quest-app-1629b.firebaseapp.com",
-  projectId: "quest-app-1629b",
-  storageBucket: "quest-app-1629b.firebasestorage.app",
-  messagingSenderId: "894667411474",
-  appId: "1:894667411474:web:8131e7b92326a3d35688cb",
-  measurementId: "G-QLG23KHB62"
+apiKey: "AIzaSyBoQlykPV5JYQPLj96D2yLpFJ0pWdtSLiU",
+authDomain: "quest-app-1629b.firebaseapp.com",
+projectId: "quest-app-1629b",
+storageBucket: "quest-app-1629b.firebasestorage.app",
+messagingSenderId: "894667411474",
+appId: "1:894667411474:web:8131e7b92326a3d35688cb"
 };
 
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
-
-getAnalytics(app);
