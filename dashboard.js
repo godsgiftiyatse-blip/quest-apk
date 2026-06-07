c828ac224005f77abe1e62598a6ece31aab84b7b
GODSGIFT IYATSE TAMARADENYEFA <godsgiftiyatse@gmail.com>
2026-06-07 11:57:18 +0100

    Update dashboard.js

--- dashboard.js
+++ dashboard.js
@@ -1,9 +1,11 @@
 import { auth } from "./firebase.js";
-import { initTasks } from "./quests.js";
 import { loadUser } from "./game.js";
+import { initTasks } from "./quests.js";
 import { updateUI } from "./ui.js";
+import { initLeaderboard } from "./leaderboard.js";
 
 auth.onAuthStateChanged(async (user) => {
+
   if (!user) {
     window.location.href = "login.html";
     return;
@@ -12,5 +14,8 @@ auth.onAuthStateChanged(async (user) => {
   const data = await loadUser();
 
   updateUI(data.level, data.xp);
+
   initTasks();
+
+  initLeaderboard();
 });
