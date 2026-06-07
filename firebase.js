e603c4bc18dfb849d448ef19cfb869711702ee7f
GODSGIFT IYATSE TAMARADENYEFA <godsgiftiyatse@gmail.com>
2026-06-07 10:53:51 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,13 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_AUTH_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
+  apiKey: "AIzaSyBoQlykPV5JYQPLj96D2yLpFJ0pWdtSLiU",
+  authDomain: "quest-app-1629b.firebaseapp.com",
+  projectId: "quest-app-1629b",
+  storageBucket: "quest-app-1629b.firebasestorage.app",
+  messagingSenderId: "894667411474",
+  appId: "1:894667411474:web:8131e7b92326a3d35688cb",
+  measurementId: "G-QLG23KHB62"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+
+getAnalytics(app);
