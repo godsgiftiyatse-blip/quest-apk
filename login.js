3df7f6f4d5765a327f5aaf9915bdab6001a0f5ee
GODSGIFT IYATSE TAMARADENYEFA <godsgiftiyatse@gmail.com>
2026-06-07 11:43:50 +0100

    Update login.js

--- login.js
+++ login.js
@@ -1,21 +1,35 @@
 import { auth } from "./firebase.js";
 import {
-  signInWithEmailAndPassword
+    signInWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
 
-document.getElementById("loginForm").addEventListener("submit", async (e) => {
+/* UI ELEMENTS */
+const form = document.getElementById("loginForm");
+const errorBox = document.getElementById("error");
+const btn = document.getElementById("loginBtn");
+
+form.addEventListener("submit", async (e) => {
     e.preventDefault();
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
+    // reset UI
+    errorBox.innerText = "";
+    btn.innerText = "Logging in...";
+    btn.disabled = true;
+
     try {
         await signInWithEmailAndPassword(auth, email, password);
 
-        // success → go dashboard
+        // success → redirect
         window.location.href = "dashboard.html";
 
     } catch (error) {
-        alert("Login failed: " + error.message);
+        errorBox.innerText = error.message;
+
+    } finally {
+        btn.innerText = "Login";
+        btn.disabled = false;
     }
 });
