a06603b604221fa952e9c4ee6b6554338eb4c23f
GODSGIFT IYATSE TAMARADENYEFA <godsgiftiyatse@gmail.com>
2026-06-07 11:37:22 +0100

    Create login.js

---
+++ login.js
@@ -0,0 +1,21 @@
+import { auth } from "./firebase.js";
+import {
+  signInWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+document.getElementById("loginForm").addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+
+    try {
+        await signInWithEmailAndPassword(auth, email, password);
+
+        // success → go dashboard
+        window.location.href = "dashboard.html";
+
+    } catch (error) {
+        alert("Login failed: " + error.message);
+    }
+});
