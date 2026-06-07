7cd9e25350eada6ca6058af1856dc56372f2436f
GODSGIFT IYATSE TAMARADENYEFA <godsgiftiyatse@gmail.com>
2026-06-07 14:14:58 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/fireba
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-firestore.js";
 
-// 🔥 YOUR FIREBASE CONFIG
+// 🔥 YOUR CONFIG
 const firebaseConfig = {
   apiKey: "AIzaSyBoQlykPV5JYQPLj96D2yLpFJ0pWdtSLiU",
   authDomain: "quest-app-1629b.firebaseapp.com",
@@ -12,9 +12,9 @@ const firebaseConfig = {
   appId: "1:894667411474:web:8131e7b92326a3d35688cb"
 };
 
-// 🚀 INIT FIREBASE
+// init Firebase
 const app = initializeApp(firebaseConfig);
 
-// 🔐 SERVICES
+// export tools
 export const auth = getAuth(app);
 export const db = getFirestore(app);
